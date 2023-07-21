488bc3188b0aed8c272c408bd07063a5880e51cd
admin <admin@DESKTOP-3NE3VV9>
2023-07-21 18:58:46 +0530

    I have created automation mini project for naukri profile updating.

---
+++ src/test/java/com/npu/test/UpdateTest.java
@@ -0,0 +1,41 @@
+package com.npu.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class UpdateTest {
+	
+	@Test
+	public void UpdateNaukriPro() throws InterruptedException {
+		WebDriver driver =  new ChromeDriver();
+		driver.manage().window().maximize();
+        driver.get("https://www.naukri.com/nlogin/login");
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.findElement(By.id("usernameField")).sendKeys("sanket.chaudhari31@gmail.com");
+        driver.findElement(By.id("passwordField")).sendKeys("Sanket@31897");
+        driver.findElement(By.xpath("//button[text()='Login']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.linkText("View profile")).click();
+        Thread.sleep(2000);
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("window.scrollBy(0,500)");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@class='widgetHead']/span[text()='editOneTheme']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(" //textarea[@id='resumeHeadlineTxt']")).sendKeys("a");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(" //textarea[@id='resumeHeadlineTxt']")).sendKeys(Keys.BACK_SPACE);
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//button[text()='Save']")).click();
+        Thread.sleep(1000);
+        driver.quit();
+        
+	}
+
+}
